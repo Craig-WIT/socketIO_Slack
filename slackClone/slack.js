7635ec1c36323c8bbdb3e51ab4cf0920a47b64db
Craig-WIT <20095351@mail.wit.ie>
2024-02-18 19:56:24 +0000

    Updating files for Slack clone

--- slackClone/slack.js
+++ slackClone/slack.js
@@ -4,13 +4,10 @@ const socketio = require('socket.io')
 
 app.use(express.static(__dirname + '/public'))
 
-const expressServer = app.listen(8000);
+const expressServer = app.listen(8001);
 const io = socketio(expressServer)
 
 io.on('connection',(socket)=>{
     console.log(socket.id,"has connected")
-    socket.emit('messageFromServer',{data:"Welcome to the socket server"})
-    socket.on('messageFromClient',(data)=>{
-        console.log(data)
-    })
+    socket.emit('welcome',"Welcome to the Server");
 })

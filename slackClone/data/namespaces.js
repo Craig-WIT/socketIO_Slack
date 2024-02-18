27020e37144fe04f789a70db4b950a21d70dd653
Craig-WIT <20095351@mail.wit.ie>
2024-02-18 20:24:55 +0000

    Setting up initial namespaces

---
+++ slackClone/data/namespaces.js
@@ -0,0 +1,16 @@
+const wikiNs = {
+    name: '/wiki',
+    image: 'https://upload.wikimedia.org/wikipedia/en/thumb/8/80/Wikipedia-logo-v2.svg/103px-Wikipedia-logo-v2.svg.png',
+}
+const mozillaNs = {
+    name: '/mozilla',
+    image: 'https://www.mozilla.org/media/img/logos/firefox/logo-quantum.9c5e96634f92.png',
+}
+const linuNs = {
+    name : '/linux',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/a/af/Tux.png',
+}
+
+const namespaces = [wikiNs,mozillaNs,linuNs];
+
+module.exports = namespaces;

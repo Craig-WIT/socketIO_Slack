29482bcf817793685c40fb7ed781de516ec07691
Craig-WIT <20095351@mail.wit.ie>
2024-02-28 21:48:25 +0000

    Handling change to namespace on server

--- slackClone/public/scripts.js
+++ slackClone/public/scripts.js
@@ -6,6 +6,21 @@ password = "password";
 
 const socket = io('http://localhost:8001');
 
+const namespaceSockets = [];
+const listeners = {
+    nsChange: [],
+}
+
+const addListeners = (nsId)=>{
+    if(!listeners.nsChange[nsId]){
+    namespaceSockets[nsId].on('nsChange',(data)=>{
+        console.log('Namespace Changed');
+        console.log(data)
+    })
+    listeners.nsChange[nsId] = true;
+}
+}
+
 socket.on('connect',()=>{
     console.log("Connected");
     socket.emit('clientConnect');
@@ -17,7 +32,13 @@ socket.on('nsList',(nsData)=>{
     nameSpacesDiv.innerHTML = "";
     nsData.forEach(ns => {
         nameSpacesDiv.innerHTML += `<div class="namespace" ns="${ns.endpoint}"><img src="${ns.image}"></div>`
-        io(`http://localhost:8001${ns.endpoint}`)
+
+        if(!namespaceSockets[ns.id]){
+            namespaceSockets[ns.id] = io(`http://localhost:8001${ns.endpoint}`)
+        }
+
+        addListeners(ns.id)
+ 
     });
 
     Array.from(document.getElementsByClassName('namespace')).forEach(element =>{
--- slackClone/slack.js
+++ slackClone/slack.js
@@ -3,11 +3,18 @@ const app = express();
 const socketio = require('socket.io');
 
 const namespaces = require('./data/namespaces');
+const Room = require('./classes/Room');
 
 app.use(express.static(__dirname + '/public'))
 
 const expressServer = app.listen(8001);
 const io = socketio(expressServer)
+
+app.get('/change-ns',(req,res)=>{
+    namespaces[0].addRoom(new Room(0,'Deleted Articles',0,false))
+    io.of(namespaces[0].endpoint).emit('nsChange',namespaces[0])
+    res.json(namespaces[0])
+})
 
 io.of("/").on('connection',(socket)=>{
     socket.emit('welcome',"Welcome to the Server");
